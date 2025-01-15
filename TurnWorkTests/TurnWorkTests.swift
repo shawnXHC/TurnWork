529529bb4898d8696017d2f2f8ac2c3eb8c1e2c9
shawn <xiaohchc@gmail.com>
2025-01-15 19:56:55 +0800

    Initial Commit

---
+++ TurnWorkTests/TurnWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  TurnWorkTests.swift
+//  TurnWorkTests
+//
+//  Created by seanxia on 2025/1/15.
+//
+
+import Testing
+@testable import TurnWork
+
+struct TurnWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
